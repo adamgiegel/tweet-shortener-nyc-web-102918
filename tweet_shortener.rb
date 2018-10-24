1371556e3b816e050b4f2516fba7895f0856e09f
Adam Giegel <adam.giegel@gmail.com>
2018-10-24 20:16:32 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -13,46 +13,41 @@ dictionary = {
 }
 end
 
-# You can use regex or, check out the solution below that
+
 # def word_substituter(tweet)
-#   tweet = tweet.dup
-#   dictionary.each do |word, replacement|
-#     if tweet.include?(word)
-#       tweet.gsub!(/\b#{word}\b/i, replacement)
+#   tweet.split.collect do |word|
+#     if dictionary.keys.include?(word.downcase)
+#       word = dictionary[word.downcase]
+#     else
+#       word
 #     end
-#   end
-#   tweet
+#   end.join(" ")
 # end
 
+# def bulk_tweet_shortener(tweets)
+#   tweets.each do |tweet|
+#     puts word_substituter(tweet)
+#   end
+# end
 
-def word_substituter(tweet)
-  tweet.split.collect do |word|
-    if dictionary.keys.include?(word.downcase)
-      word = dictionary[word.downcase]
-    else
-      word
-    end
-  end.join(" ")
-end
+# def selective_tweet_shortener(tweet)
+#   if tweet.length > 140
+#     word_substituter(tweet)
+#   else
+#     tweet
+#   end
+# end
 
-def bulk_tweet_shortener(tweets)
-  tweets.each do |tweet|
-    puts word_substituter(tweet)
-  end
-end
+# def shortened_tweet_truncator(tweet)
+#   if word_substituter(tweet).length > 140
+#     word_substituter(tweet)[0..136] + '...'
+#   else
+#     tweet
+#   end
+# end
 
-def selective_tweet_shortener(tweet)
-  if tweet.length > 140
-    word_substituter(tweet)
-  else
-    tweet
-  end
-end
-
-def shortened_tweet_truncator(tweet)
-  if word_substituter(tweet).length > 140
-    word_substituter(tweet)[0..136] + '...'
-  else
-    tweet
-  end
-end
+def word_substituter(string)
+  new_array = string.to_a 
+  new_array.each do |word|
+    if word == dictionary["key"]
+      word == dictionary[]
